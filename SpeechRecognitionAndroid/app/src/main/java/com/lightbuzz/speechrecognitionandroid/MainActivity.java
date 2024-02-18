f02364078f476c489a2aa35440e5dce2c0fa60ed
Orlando Ding <xiandao.airs@gmail.com>
2024-02-18 13:04:42 -0800

    orlando - for updates of settings of submodule

--- SpeechRecognitionAndroid/app/src/main/java/com/lightbuzz/speechrecognitionandroid/MainActivity.java
+++ SpeechRecognitionAndroid/app/src/main/java/com/lightbuzz/speechrecognitionandroid/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
